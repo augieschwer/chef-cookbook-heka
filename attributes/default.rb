1407cf037cd112ebe8e27693ece49d9e35223993
dcouture <dcouture@dyn.com>
2014-07-09 11:23:57 -0400

    Move values into attributes to allow for overrides

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,4 +9,10 @@ default['heka']['download']['mirror'] = 'https://github.com/mozilla-services/hek
 default['heka']['download']['version'] = '0.5.1'
 default['heka']['download']['arch'] = 'amd64'
 default['heka']['download']['extension'] = 'deb'
-
+default['heka']['download']['remote_src'] = "#{node['heka']['download']['mirror']}#{node['heka']['download']['version']}/heka_#{node['heka']['download']['version']}_#{node['heka']['download']['arch']}.#{node['heka']['download']['extension']}"
+default['heka']['download']['remote_file'] = ::File.join("/tmp", "heka_#{node['heka']['download']['version']}_#{node['heka']['download']['arch']}.#{node['heka']['download']['extension']}")
+default['heka']['bin'] = '/usr/bin/hekad'
+default['heka']['init'] = '/etc/init/heka.conf'
+default['heka']['dir'] = '/etc/heka'
+default['heka']['conf_dir'] = '/etc/heka/'
+default['heka']['log'] = '/var/log/hekad.log'
